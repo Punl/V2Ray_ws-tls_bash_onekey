e9ba50228742fa2dff9975d3de0205a68a09a34f
Punl <62181328+Punl@users.noreply.github.com>
2020-04-19 21:11:58 +0800

    Update install_localgo.sh

--- install_localgo.sh
+++ install_localgo.sh
@@ -415,7 +415,7 @@ ssl_install() {
     fi
     judge "安装 SSL 证书生成脚本依赖"
 
-    curl https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh | sh
+    curl https://raw.githubusercontent.com/acmesh-official/acme.sh/$BRANCH/acme.sh | INSTALLONLINE=1  sh
     judge "安装 SSL 证书生成脚本"
 }
 domain_check() {
